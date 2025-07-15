e4e708f357e48de21c9da05852fa3fb7ece2fad1
rayyan-0 <rayyanalmalki1@gmail.com>
2025-07-15 16:34:08 +0300

    Added view all current task functionality + improved UI.

--- dist/script.js
+++ dist/script.js
@@ -6,9 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const prompt_sync_1 = __importDefault(require("prompt-sync"));
 const prompt = (0, prompt_sync_1.default)();
 let exit = false;
+console.log("press A to add a new task. V to see current tasks. type exit to, well, exit.\n");
+const tasks = [];
 do {
-    const input = prompt("press A to add a new task. V to see current tasks: ");
-    const tasks = [];
+    const input = prompt("");
     const add_new_task = () => {
         let newTask = {
             name: prompt("Please enter a task name: "),
@@ -17,14 +18,28 @@ do {
             isImportant: prompt("Important? true/false: ") === "true",
         };
         tasks.push(newTask);
-        console.log(`New task with name ${newTask.name} has been added. Click V to see list of all tasks. `);
+        console.log(`
+==================================================
+
+✅ New task with name ${newTask.name} added successfully!
+
+==================================================
+        `);
     };
     if (input === "A") {
         add_new_task();
     }
     if (input === "V") {
-        console.log("Here's a list of all your current tasks. To enter a specific task, just write its name");
-        let taskList = tasks.forEach((task) => console.log(task.name));
+        console.log(`
+==================================================
+
+📋 Here's a list of all your current tasks.
+
+To enter a specific task, just write its name.
+
+==================================================
+`);
+        let taskList = tasks.forEach((task) => console.log(`Name: ${task.name} \nDesc: ${task.description} \nis Urgent: ${task.isUrgent} \nisImportant: ${task.isImportant} \n\n===================================================== \n`));
         console.log(taskList);
     }
     if (input === "exit") {
--- src/script.ts
+++ src/script.ts
@@ -4,17 +4,19 @@ const prompt = promptSync()
 
 let exit: boolean = false; 
 
+type task = {
+    name: string | number
+    description: string | number
+    isUrgent: boolean
+    isImportant: boolean
+}
+    
+console.log("press A to add a new task. V to see current tasks. type exit to, well, exit.\n");
+
+const tasks: task[] = [];
+
 do {
-    const input = prompt("press A to add a new task. V to see current tasks: ")
-    
-    type task = {
-        name: string | number
-        description: string | number
-        isUrgent: boolean
-        isImportant: boolean
-    }
-    
-    const tasks: task[] = [];
+    const input = prompt("")
     
     const add_new_task = () => {
     
@@ -25,21 +27,44 @@ do {
         isImportant: prompt("Important? true/false: ") === "true",}
     
      tasks.push(newTask);
-     console.log(`New task with name ${newTask.name} has been added. Click V to see list of all tasks. `)
+     console.log(`
+==================================================
+
+✅ New task with name ${newTask.name} added successfully!
+
+==================================================
+        `);
     }
     
-    if (input === "A") {
+    if (input === "a") {
         add_new_task();
     }
     
-    if (input === "V") {
-        console.log("Here's a list of all your current tasks. To enter a specific task, just write its name")
-        let taskList = tasks.forEach((task) => console.log(task.name))
+    if (input === "v") {
+        console.log(`
+==================================================
+
+📋 Here's a list of all your current tasks.
+
+To enter a specific task, just write its name.
+
+==================================================
+`);
+        let taskList = tasks.forEach((task) => console.log(`Name: ${task.name} \nDesc: ${task.description} \nis Urgent: ${task.isUrgent} \nisImportant: ${task.isImportant} \n\n===================================================== \n`))
         console.log(taskList)
     }
 
         if (input === "exit") {
         exit = true;
     }
+    
+        // if (input === "e") {
+        //     let userSearch = prompt("Please provide the name of the task you want to edit")
+        //     tasks.map((task) => {
+        //         if (task.name == userSearch) {
+        //             console.log("Select what you want to ")
+        //         }
+        //     })
+        // }
 
 } while (exit == false)
